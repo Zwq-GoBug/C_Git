15486c7a63e2cf56bd72e3735d820966ca562b32
ZWQ-GoBug <2210830440@qq.com>
2018-06-05 19:35:54 +0800

    增加C的深度解剖文档！

--- 结构体_基础/结构体_基础/基础.c
+++ 结构体_基础/结构体_基础/基础.c
@@ -10,5 +10,12 @@ typedef struct EX
 
 int main(void)
 {
-	
+	int a[10] = { 0,1,2,3,4,8,7,-8,10,-1 };
+	int *ptr = a;
+	for (int i = 0;i < 10;i++)
+	{
+		printf("a[%d]: %d\n",i,*(ptr+i));
+		//ptr++;
+	}
+	return 0;
 }
